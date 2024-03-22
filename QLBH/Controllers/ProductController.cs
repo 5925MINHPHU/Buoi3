3eff123b0ab9c5f7efee51985020671d3003b514
Administrator <Administrator@10PC323>
2024-03-22 16:14:24 +0700

    Add project files.

---
+++ QLBH/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using QLBH.Models;
+
+namespace QLBH.Controllers 
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+        // Hiển thị danh sách sản phẩm
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return View(products);
+        }
+
+        // Hiển thị form thêm sản phẩm mới
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+
+        // Xử lý thêm sản phẩm mới
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productRepository.AddAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            // Nếu ModelState không hợp lệ, hiển thị form với dữ liệu đã nhập
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View(product);
+        }
+    }
+}
---
+++ QLBH/Models/EFCategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace QLBH.Models
+{
+    public class EFCategoryRepository: ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EFCategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public Task<Category> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
